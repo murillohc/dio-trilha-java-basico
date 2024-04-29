92a4b56d131b64f6fededa04b256aee31ec5a874
Murillo Henrique <murillohenriquecoelho@gmail.com>
2024-04-29 16:22:20 -0300

    Add: Projeto BancoDigital, java básico DIO

--- projetos-java/banco-digital/src/BancoDigital.java
+++ projetos-java/banco-digital/src/BancoDigital.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class BancoDigital {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Seja bem vindo(a) ao banco MHC!");
+        System.out.println("Digite seu nome: ");
+        String nome= ler.nextLine();
+        System.out.println("Digite sua agência: ");
+        String agencia= ler.nextLine();
+        System.out.println("Insira seu número de usuário: ");
+        int numero= ler.nextInt();
+        System.out.println("Insira o valor em R$ que deseja depositar: ");
+        Double saldo= ler.nextDouble();
+
+        System.out.println("Olá " + nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero +" e seu saldo " + saldo +" já está disponível para saque");
+
+        
     }
 }
