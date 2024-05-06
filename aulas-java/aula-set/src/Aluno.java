392883f41f077544c29b30191a9cd85942bbbc84
Murillo Henrique <murillohenriquecoelho@gmail.com>
2024-05-06 19:32:47 -0300

    feat: aula set, comparator não funcionando

---
+++ aulas-java/aula-set/src/Aluno.java
@@ -0,0 +1,61 @@
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno> {
+    private String nome;
+    private double media;
+    private long matricula;
+    
+    public Aluno(String nome, double media, long matricula) {
+        this.nome = nome;
+        this.media = media;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno [nome=" + nome + ", nota=" + media + ", matricula=" + matricula + "]";
+    }
+
+     @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Aluno aluno)) return false;
+    return Objects.equals(getMatricula(), aluno.getMatricula());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getMatricula());
+  }
+
+    @Override
+    public int compareTo(Aluno aluno) {
+        // TODO Auto-generated method stub
+        return nome.compareTo(aluno.getNome());
+    }
+
+    class ComparatorNota implements Comparator<Aluno> {
+        @Override
+        public int compare(Aluno o1, Aluno o2) {
+          return Double.compare(o1.getMedia(), o2.getMedia());
+        }
+    
+
+    
+
+
+}
+}
